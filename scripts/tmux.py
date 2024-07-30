2a680691a6d5c5f5f20b47d05ecde50c3354194c
Bryan <bryank.b96@gmail.com>
2024-07-30 21:44:29 +0200

    Add get_option method for getting options set in tmux config

--- scripts/tmux.py
+++ scripts/tmux.py
@@ -1,4 +1,4 @@
-from scripts.utils import simple_proc
+from scripts.utils import simple_proc, simple_read
 
 
 def set_global(option, value):
@@ -7,3 +7,12 @@ def set_global(option, value):
 
 def set_window_global(option, value):
     simple_proc(["tmux", "set-option", "-w", "-g", option, value])
+
+
+def get_option(option: str, default="") -> str:
+    proc = simple_proc(["tmux", "show-option", "-gqv", f"@maatwerklijn_{option}"])
+    text = simple_read(proc).decode("utf-8")
+    if not bool(text):
+        return default
+
+    return text
